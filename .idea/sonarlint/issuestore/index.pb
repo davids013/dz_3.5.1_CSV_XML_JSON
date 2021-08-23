43a495a842fcb5e19a3850e79dc29eeff639fa62
Vitaly Shvedko <davids013013@gmail.com>
2021-08-23 22:18:58 +0300

    Complete task 1

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+7
+Task.md,7\c\7c884afdace8becd049ba68a0aac5f6aa2abb824
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,27 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.*;
 import java.util.List;
 
 public class Main {
+    private static final String SEP = File.separator;
     public static void main(String[] args) {
         System.out.println("\tДомашнее задание к занятию 1.5: Работа с файлами CSV, XML, JSON\n" +
                 "\tЗадача 1: CSV - JSON парсер\n");
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
+        String fileName = "src" + SEP + "main" + SEP + "resources" + SEP + "data.csv";
+        String ouputFileName = "output.dat";
         List<Employee> employees = parseCSV(columnMapping, fileName);
         employees.forEach(System.out::println);
         String json = listToJson(employees);
         System.out.println(json);
-
+        writeString(json, ouputFileName);
     }
 
     private static List<Employee> parseCSV(String[] fields, String fileName) {
@@ -43,7 +41,24 @@ public class Main {
     }
 
     private static String listToJson(List<Employee> employees) {
-        String json = null;
+        String json = new String();
+        Gson gson = new GsonBuilder().create();
+        String[] tempArray = gson.toJson(employees).split(",");
+        for (String s : tempArray) {
+            json += s + ",\r\n";
+        }
         return json;
+    }
+
+    private static boolean writeString(String s, String filePath) {
+        boolean isWrited = false;
+        try (FileWriter writer = new FileWriter(filePath, false)) {
+            writer.write(s);
+            writer.flush();
+            isWrited = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isWrited;
     }
 }
