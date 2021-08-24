e003fe11956e751f86024cace1e6be6e50b0992a
Vitaly Shvedko <davids013013@gmail.com>
2021-08-24 21:04:48 +0300

    Add .prettyPrinting to JSON string

--- core/src/main/java/Methods.java
+++ core/src/main/java/Methods.java
@@ -1,18 +1,17 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.List;
 
 abstract class Methods {
     public static String listToJson(List<Employee> employees) {
-        String json = new String();
-        Gson gson = new GsonBuilder().create();
-        String[] tempArray = gson.toJson(employees).split(",");
-        for (String s : tempArray) {
-            json += s + ",\r\n";
-        }
-        json = json.substring(0, json.length() - 3);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        Type listType = new TypeToken<List<Employee>>() {}.getType();
+//        String json = gson.toJson(employees, listType);
+        String json = gson.toJson(employees);
         System.out.println("\t>Список сотрудников переведен в строку JSON");
         return json;
     }
